4ff02da0556dfe3bd72015ce8d3f2c6b75d04397
hoyeon96 <hoyen96@naver.com>
2021-06-01 16:17:54 +0900

    기본 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hello from './hello';
+import Wrapper from './Wrapper';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontsize : 24,
+    padding : '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   {/* 안녕하세용 */}
+    //   {// 안녕하세용 
+    //   }
+
+
+    //   <Hello name = '일번이'/>
+    //   <Hello name = '이번이' color='pink'/>
+    //   <Hello name = '삼번이'/>
+    //   <Hello name = 'react' color='red'/>
+    //   <div style = {style}>{name}</div>
+    //   <div className = {'gray-box'}>{name}</div>
+    //   <Hello />
+    //   <Hello />
+    //   <Hello />
+    //   <Hello />
+    // </div>
+
+    <Wrapper>
+      <Hello name = '일번이' color = 'red'/>
+      <Hello color = 'blue' />
+    </Wrapper>
   );
 }
+
 
 export default App;
